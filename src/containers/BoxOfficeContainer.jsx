72e37d90eddaee91e99a3a7a629733e27c322ecf
lerrybe <lerry597@gmail.com>
2022-02-28 06:06:16 +0900

    [refactor] 박스오피스 페이지 리팩토링

--- src/containers/BoxOfficeContainer.jsx
+++ src/containers/BoxOfficeContainer.jsx
@@ -1,37 +1,22 @@
 import { useSelector } from 'react-redux';
 
-import { Link } from 'react-router-dom';
-
-import styled from '@emotion/styled';
-
 import { get } from '../utils/utils';
+
+import SelectMovies from '../components/SelectMovies';
 
 import boxoffice from '../img/boxoffice.svg';
 
 import ContainerWrapDiv from '../styles/ContainerWrapDiv';
 import SubIconImg from '../styles/SubIconImg';
-import { ContentsTitleDiv, ContentsTitleWrapDiv } from '../styles/ContentsTitleDiv';
-import SelectMovies from '../components/SelectMovies';
-
-const InnerContainerDiv = styled.div`
-  box-sizing: border-box;
-  padding-left: 10px;
-`;
-
-const MovieList = styled.li`
-  font-size: 25px;
-  font-weight: 600;
-  font-family: DungGeunMo;
-  color: #d7dfeb;
-  margin-bottom: 23px;
-`;
-
-const StyledLink = styled(Link)`
-  color: #d7dfeb;
-  &:hover {
-    color: #FFD717;
-  }
-`;
+import {
+  ContentsTitleDiv,
+  ContentsTitleWrapDiv,
+} from '../styles/ContentsTitleDiv';
+import {
+  InnerContainerDiv,
+  MovieList,
+  StyledLink,
+} from '../styles/BoxOffice';
 
 export default function BoxOfficeContainer() {
   const dailyBoxOfficeList = useSelector(get('dailyBoxOfficeList'));
@@ -50,7 +35,7 @@ export default function BoxOfficeContainer() {
           <ol>
             {dailyBoxOfficeList.map((movie) => (
               <MovieList key={movie.rank}>
-                <StyledLink to={`/show/${movie.movieNm.replace(/ /gi, '')}`}>
+                <StyledLink to={`/show/${encodeURI(movie.movieNm.replace(/ /gi, ''))}`}>
                   {`${movie.rank}) ${movie.movieNm}`}
                 </StyledLink>
               </MovieList>
--- src/pages/BoxOfficePage.jsx
+++ src/pages/BoxOfficePage.jsx
@@ -3,12 +3,15 @@ import styled from '@emotion/styled';
 import NavigationBar from '../components/NavigationBar';
 import PlayDevice from '../components/PlayDevice';
 import BoxOfficeContainer from '../containers/BoxOfficeContainer';
+import { clearItem } from '../_services/storage';
 
 const BoxOfficeWrapDiv = styled.div`
   display: flex;
 `;
 
 export default function BoxOfficePage() {
+  clearItem('movieInfo');
+
   return (
     <BoxOfficeWrapDiv>
       <NavigationBar />
